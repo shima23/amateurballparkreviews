433756e9230d4a4ff85cf503aa2050f3802f31cc
shima23 <manaka123480@yahoo.co.jp>
2020-10-22 23:58:43 +0900

    会員登録・完了画面、ログイン画面作成（ハリボテ）

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
 <template>
   <v-app dark>
-    <v-app-bar :clipped-left="clipped" dense fixed app dark　elevation="0">
+    <v-app-bar :clipped-left="clipped" dense fixed app dark elevation="0">
       <v-toolbar-title v-text="title" />
       <v-spacer />
+      <v-toolbar-items>
+        <v-btn v-for="(item, i) in items" :key="i" :to="item.to" elevation="0">
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
     <v-main>
       <nuxt />
@@ -27,6 +32,12 @@ export default class Default extends Vue {
   private right: boolean = true
   private rightDrawer: boolean = false
   private title: string = 'Amateur Ballpark Reviews'
+
+  private items = [
+    { title: '会員登録', to: '/account/register'},
+    { title: 'ログイン', to: '/login'}
+  ]
+
 }
 </script>
 <style>
