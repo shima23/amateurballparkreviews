efbea3ff1cf4730dba7d3c4ced446b830137ffbf
shima23 <manaka123480@yahoo.co.jp>
2020-12-01 16:51:32 +0900

    headerにtokenを含める実装のお試し

--- components/molecules/MyPage/ChangePassword.vue
+++ components/molecules/MyPage/ChangePassword.vue
@@ -4,32 +4,43 @@
       span パスワード変更
     div.-input-password
       p 現在のパスワード
-        v-text-field(v-model="mailAddress" :type="'password'")
+        v-text-field(v-model="reqDto.oldPassword" :type="'password'")
       p 新しいパスワード
-        v-text-field(v-model="mailAddress" :type="'password'")
+        v-text-field(v-model="reqDto.newPassword" :type="'password'")
     div
-      v-btn(elevation="1" @click="click()") 変更する
+      v-btn(elevation="1" @click="chengePassword()") 変更する
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
+import axios from 'axios'
 
 @Component({})
 export default class ChangePassword extends Vue {
-  data() {
-    return {}
+  private reqDto = {
+    oldPassword: '',
+    newPassword: '',
   }
 
-  private click() {}
+  private async chengePassword() {
+    const url = 'http://0.0.0.0:8080/password/change'
+    var config = {
+      headers: {
+        Authorization: this.$auth.getToken('local'),
+      },
+    }
+
+    const response = await axios.post(url, this.reqDto, config)
+  }
 }
 </script>
 <style lang="sass" scoped>
 .-change-password
-    padding: 20px
+    padding: 24px
     background: #ffffff
 .-title
     font-size: 24px !important
-    padding-bottom: 10px
+    padding-bottom: 12px
     border-bottom: 1px #e0e0e0 solid
 .-input-password
     padding: 16px 0
